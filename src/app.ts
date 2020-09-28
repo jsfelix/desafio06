34223e117254920118b2aa4160825b5a86a4b8a0
Jefferson Felix <jsfelix@gmail.com>
2020-09-28 16:58:58 -0300

    Habilita o CORS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import 'cors';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -14,6 +14,7 @@ createConnection();
 const app = express();
 const log = console;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
